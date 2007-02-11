f14799c1b49694f12ba7e0853ca8c385600a8640
Erick Gallesio <eg@essi.fr>
2007-02-11 20:59:38 +0100

    New mechanism WHEN-COMPILE which is evalueated only when compiling

--- src/fport.c
+++ src/fport.c
@@ -21,7 +21,7 @@
  *
  *           Author: Erick Gallesio [eg@unice.fr]
  *    Creation date:  8-Jan-2000 14:48 (eg)
- * Last file update:  9-Feb-2007 16:15 (eg)
+ * Last file update:  9-Feb-2007 17:02 (eg)
  *
  * This implementation is built by reverse engineering on an old SUNOS 4.1.1
  * stdio.h. It has been simplified to fit the needs for STklos. In particular
