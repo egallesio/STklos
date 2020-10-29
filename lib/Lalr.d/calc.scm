b1cddb83acd431a11cd232c2a1cc21f79544d588
Erick Gallesio <eg@unice.fr>
2020-10-29 18:08:00 +0100

    Changed the version of LALR-SCM to version 2.5.0

--- lib/Lalr.d/calc.scm
+++ lib/Lalr.d/calc.scm
@@ -2,13 +2,6 @@
 ;;;; Simple calculator in Scheme
 ;;;
 ;;
-;; @created   "Tue Jan  6 12:47:23 2004"
-;; @modified  "Mon Oct 25 11:07:24 2004"
-;; @author    "Dominique Boucher"
-;; @copyright "Dominique Boucher"
-;;
-;; Simple arithmetic calculator. 
-;; 
 ;;   This program illustrates the use of the lalr-scm parser generator
 ;; for Scheme. It is NOT robust, since calling a function with 
 ;; the wrong number of arguments may generate an error that will
@@ -22,55 +15,50 @@
 
 (define calc-parser
   (lalr-parser
-   
+
    ;; --- Options 
    ;; output a parser, called calc-parser, in a separate file - calc.yy.scm, 
-   ;(output:    calc-parser "calc.yy.scm")
+   ;; STklos:
+   ;;(output:    calc-parser "calc.yy.scm")
    ;; output the LALR table to calc.out
-   ;(out-table: "calc.out")
+   ;;STklos
+   ;;(out-table: "calc.out")
    ;; there should be no conflict
-   (expect:    0)
-   
+   (expect:    5)
+
    ;; --- token definitions
    (ID NUM = LPAREN RPAREN NEWLINE COMMA
-    (left: + -)
-    (left: * /)
-    (nonassoc: uminus))
-   
+       (left: + -)
+       (left: * /)
+       (nonassoc: uminus))
+
    (lines    (lines line) : (display-result $2)
-	     (line)       : (display-result $1))
-	   
+             (line)       : (display-result $1))
+
 
    ;; --- rules
    (line     (assign NEWLINE)        : $1
-	     (expr   NEWLINE)        : $1
-	     (error  NEWLINE)        : #f)
-   
+             (expr   NEWLINE)        : $1
+	     (NEWLINE)               : #f
+             (error  NEWLINE)        : #f)
+
    (assign   (ID = expr)             : (add-binding $1 $3))
-   
+
    (expr     (expr + expr)           : (+ $1 $3)
-	     (expr - expr)           : (- $1 $3)
-	     (expr * expr)           : (* $1 $3)
-	     (expr / expr)           : (/ $1 $3)
-	     (- expr (prec: uminus)) : (- $2)
-	     (ID)                    : (get-binding $1)
-	     (ID LPAREN args RPAREN) : (invoke-proc $1 $3)
-	     (NUM)                   : $1
-	     (LPAREN expr RPAREN)    : $2)
-    
+             (expr - expr)           : (- $1 $3)
+             (expr * expr)           : (* $1 $3)
+             (expr / expr)           : (/ $1 $3)
+             (- expr (prec: uminus)) : (- $2)
+             (ID)                    : (get-binding $1)
+             (ID LPAREN args RPAREN) : (invoke-proc $1 $3)
+             (NUM)                   : $1
+             (LPAREN expr RPAREN)    : $2)
+
    (args     ()                      : '()
-	     (expr arg-rest)         : (cons $1 $2))
-   
+             (expr arg-rest)         : (cons $1 $2))
+
    (arg-rest (COMMA expr arg-rest)   : (cons $2 $3)
-	     ()                      : '())))
-
-
-(define (display-result v)
-  (if v
-      (begin
-	(display "==> ")
-	(display v)
-	(newline))))
+             ()                      : '())))
 
 
 ;;;
@@ -78,57 +66,77 @@
 ;;;
 
 
+(cond-expand
+ (gambit
+  (define port-line input-port-line)
+  (define port-column input-port-column))
+ 
+ (chicken
+  (define (force-output) #f)
+  (define (port-line port) 
+    (let-values (((line _) (port-position port)))
+      line))
+  
+  (define (port-column port)
+    (let-values (((_ column) (port-position port)))
+      column)))
+
+ (guile
+  (define (port-line port) '??)
+  (define (port-column port) '??))
+ 
+ (else
+  (define (force-output) #f)
+  (define (port-line port) '??)
+  (define (port-column port) '??)))
+
+
 (define (make-lexer errorp)
   (lambda ()
     (letrec ((skip-spaces
-	      (lambda ()
-		(let loop ((c (peek-char)))
-		  (if (and (not (eof-object? c))
-			   (or (char=? c #\space) (char=? c #\tab)))
-		      (begin
-			(read-char)
-			(loop (peek-char)))))))   
-	     (read-number 
-	      (lambda (l)
-		(let ((c (peek-char)))
-		  (if (char-numeric? c)
-		      (read-number (cons (read-char) l))
-		      (string->number (apply string (reverse l)))))))
-	     (read-id
-	      (lambda (l)
-		(let ((c (peek-char)))
-		  (if (char-alphabetic? c)
-		      (read-id (cons (read-char) l))
-		      (string->symbol (apply string (reverse l))))))))
+              (lambda ()
+                (let loop ((c (peek-char)))
+                  (if (and (not (eof-object? c))
+                           (or (char=? c #\space) (char=? c #\tab)))
+                      (begin
+                        (read-char)
+                        (loop (peek-char)))))))
+             (read-number
+              (lambda (l)
+                (let ((c (peek-char)))
+                  (if (char-numeric? c)
+                      (read-number (cons (read-char) l))
+                      (string->number (apply string (reverse l)))))))
+             (read-id
+              (lambda (l)
+                (let ((c (peek-char)))
+                  (if (char-alphabetic? c)
+                      (read-id (cons (read-char) l))
+                      (string->symbol (apply string (reverse l))))))))
 
       ;; -- skip spaces
       (skip-spaces)
       ;; -- read the next token
-      (let loop ((c (read-char)))
-	(cond
-	 ((eof-object? c)      '*eoi*)
-	 ((char=? c #\newline) 'NEWLINE)
-	 ((char=? c #\+)       '+)
-	 ((char=? c #\-)       '-)
-	 ((char=? c #\*)       '*)
-	 ((char=? c #\/)       '/)
-	 ((char=? c #\=)       '=)
-	 ((char=? c #\,)       'COMMA)
-	 ((char=? c #\()       'LPAREN)
-	 ((char=? c #\))       'RPAREN)
-	 ((char-numeric? c)    (cons 'NUM (read-number (list c))))
-	 ((char-alphabetic? c) (cons 'ID  (read-id (list c))))
-	 (else                 
-	  (errorp "PARSE ERROR : illegal character: " c)
-	  (skip-spaces)
-	  (loop (read-char))))))))
+      (let loop ()
+        (let* ((location (make-source-location "*stdin*" (port-line (current-input-port)) (port-column (current-input-port)) -1 -1))
+               (c (read-char)))
+          (cond ((eof-object? c)      '*eoi*)
+                ((char=? c #\newline) (make-lexical-token 'NEWLINE location #f))
+                ((char=? c #\+)       (make-lexical-token '+       location #f))
+                ((char=? c #\-)       (make-lexical-token '-       location #f))
+                ((char=? c #\*)       (make-lexical-token '*       location #f))
+                ((char=? c #\/)       (make-lexical-token '/       location #f))
+                ((char=? c #\=)       (make-lexical-token '=       location #f))
+                ((char=? c #\,)       (make-lexical-token 'COMMA   location #f))
+                ((char=? c #\()       (make-lexical-token 'LPAREN  location #f))
+                ((char=? c #\))       (make-lexical-token 'RPAREN  location #f))
+                ((char-numeric? c)    (make-lexical-token 'NUM     location (read-number (list c))))
+                ((char-alphabetic? c) (make-lexical-token 'ID      location (read-id (list c))))
+                (else
+                 (errorp "PARSE ERROR : illegal character: " c)
+                 (skip-spaces)
+                 (loop))))))))
 
-
-(define (read-line)
-  (let loop ((c (read-char)))
-    (if (and (not (eof-object? c))
-	     (not (char=? c #\newline)))
-	(loop (read-char)))))
 
 
 ;;;
@@ -155,25 +163,38 @@
 
 (define (get-binding var)
   (let ((p (assq var *env*)))
-    (if p 
-	(cdr p)
-	0)))
+    (if p
+        (cdr p)
+        0)))
 
 
 (define (invoke-proc proc-name args)
   (let ((proc (get-binding proc-name)))
     (if (procedure? proc)
-	(apply proc args)
-	(begin
-	  (display "ERROR: invalid procedure:")
-	  (display proc-name)
-	  (newline)
-	  0))))
+        (apply proc args)
+        (begin
+          (display "ERROR: invalid procedure:")
+          (display proc-name)
+          (newline)
+          0))))
 
 
 ;;;
 ;;;;   The main program
 ;;;
+
+
+(define (display-result v)
+  (if v
+      (begin
+        (display v)
+        (newline)))
+  (display-prompt))
+
+
+(define (display-prompt)
+  (display "[calculator]> ")
+  (force-output))
 
 
 (define calc
@@ -188,14 +209,33 @@
        (display "* exit.                        *") (newline)
        (display "********************************") (newline)
        (init-bindings)
-       (add-binding 'quit k)
+       (add-binding 'quit (lambda () (k #t)))
        (letrec ((errorp
-		 (lambda args
-		   (for-each display args) (newline)))
-		(start
-		 (lambda ()
-		   (calc-parser (make-lexer errorp) errorp))))
-	 (start))))))
-
-(calc)
+                 (lambda (message . args)
+                   (display message)
+                   (if (and (pair? args) 
+                            (lexical-token? (car args)))
+                       (let ((token (car args)))
+                         (display (or (lexical-token-value token)
+                                      (lexical-token-category token)))
+                         (let ((source (lexical-token-source token)))
+                           (if (source-location? source)
+                               (let ((line (source-location-line source))   
+				     (column (source-location-column source)))
+				 (if (and (number? line) (number? column))
+				     (begin
+				       (display " (at line ")
+				       (display line)
+				       (display ", column ")
+				       (display (+ 1 column))
+				       (display ")")))))))
+                       (for-each display args))
+                   (newline)))
+                (start
+                 (lambda ()
+                   (calc-parser (make-lexer errorp) errorp))))
+	 (display-prompt)
+         (start))))))
+;; STklos
+;; (calc)
 
