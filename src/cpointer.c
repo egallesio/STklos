c686cde18ebd2509b2d74906c6fa66a0f51591d0
Jeronimo Pellegrini <j_p@aleph0.info>
2021-12-01 20:12:43 -0300

    Document cpointer procedures
    Still missing: cpointer-type, cpointer-type-set!,
    cpointer-data-set!

--- src/cpointer.c
+++ src/cpointer.c
@@ -45,10 +45,19 @@ SCM STk_make_Cpointer(void *ptr, SCM type, SCM data)
   return z;
 }
 
+/*
+<doc cpointer?
+ * (cpointer? obj)
+ *
+ * Returns |#t| is |obj| is a cpointer (an encapsulated pointer to
+ * a C object), and |#f| otherwise. Cpointers are used by the C FFI.
+doc>
+ */
 DEFINE_PRIMITIVE("cpointer?", cpointerp, subr1, (SCM obj))
 {
   return MAKE_BOOLEAN(CPOINTERP(obj));
 }
+
 
 DEFINE_PRIMITIVE("cpointer-type", cpointer_type, subr1, (SCM obj))
 {
@@ -56,6 +65,34 @@ DEFINE_PRIMITIVE("cpointer-type", cpointer_type, subr1, (SCM obj))
   return CPOINTER_TYPE(obj);
 }
 
+/*
+<doc EXT allocate-bytes cpointer-data cpointer->string
+ * (allocate-bytes n)
+ * (cpointer-data obj)
+ * (cpointer->string obj)
+ *
+ * |Allocate-bytes| will allocate |n| consecutive bytes using |malloc|,
+ * and that region will consequently not be managed by the STklos GC.
+ * It returns a |cpointer| Schem object that points to the frst byte
+ * allocated.
+ *
+ * |Cpointer-data| returns the data to which the cpointer |obj| points.
+ * The result is not a printable Scheme object, and if it is a string,
+ * then |cpointer->string| converts it into a Scheme string.
+ *
+ * The use case for these procedures is the STklos C FFI:
+ * @lisp
+ * (define-external c-ghn ((s :pointer) (size :int))
+ *                  :entry-name "gethostname"
+ *                  :return-type :int)
+ * (define name (allocate-bytes 10))
+ * 
+ * name                    => #[C-pointer 7fd830820f80 @ 7fd8305bee40]
+ * (c-ghn name 9)          => 0
+ * (cpointer->string name) => "socrates"
+ * @end lisp
+doc>
+ */
 DEFINE_PRIMITIVE("cpointer-data", cpointer_data, subr1, (SCM obj))
 {
   if (! CPOINTERP(obj)) error_bad_cpointer(obj);
@@ -114,6 +151,24 @@ DEFINE_PRIMITIVE("allocate-bytes", allocate_bytes, subr1, (SCM sz))
   return z;
 }
 
+/*
+<doc EXT free-bytes
+ * (free-bytes obj)
+ *
+ * |Obj| must be a |cpointer| to allocated data. When |Free-bytes| is
+ * called on |obj|, it will deallocate its data (calling the C function
+ * |free|).
+ *
+ * @lisp
+ * (define a (allocate-bytes 10))
+ * a   => #[C-pointer 7fd91e8e0f80 @ 7fd91e897b70]
+ * (free-bytes a)
+ * a   => #[C-pointer 0 @ 7fd91e897b70]
+ * @end lisp
+ *
+ * In the second time that |a| is printed, the first number shown is zero,
+ * indicating that its data pointer does not point to allocated memory.
+ */
 DEFINE_PRIMITIVE("free-bytes", free_bytes, subr1, (SCM p))
 {
   if (!CPOINTERP(p))
