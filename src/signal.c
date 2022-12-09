755228dfa7893ecac67d2137e3a265fabe634044
Erick Gallesio <eg@stklos.net>
2022-12-09 18:25:18 +0100

    Added fucntions set-signal-handler! and get-signal-handler
    Need to define fast paths for standard signals???

--- src/signal.c
+++ src/signal.c
@@ -22,20 +22,43 @@
  *
  *           Author: Erick Gallesio [eg@unice.fr]
  *    Creation date: 10-Oct-1995 07:55
- * Last file update:  4-Dec-2022 21:47 (eg)
+ * Last file update:  9-Dec-2022 18:34 (eg)
  *
  */
 
 #include "stklos.h"
 #include <signal.h>
 
+#ifdef SA_RESTART
+#  define SA_FLAGS SA_RESTART
+#else
+#  define SA_FLAGS 0
+#endif
+
+/* NSIG is not POSIX, but it seems to be always defined. Anyway....  */
+#ifndef NSIG
+#  define NSIG 65
+#endif
+
+/*
+ * Definition of the signal table
+ *
+ * Convention: signals[i] can be
+ *     - #f if signal is ignored
+ *     - #t if signal is set to default
+ *     - a Scheme procedure
+ */
+static SCM signals[NSIG];
+
+
+/* ====================================================================== */
 struct signal_info {
   char *name;
   int value;
 };
 
 
-static struct signal_info signal_set[] = {
+static struct signal_info signal_names[] = {
   /* Posix.1 signals */
   {"SIGHUP",    SIGHUP},
   {"SIGINT",    SIGINT},
@@ -115,6 +138,12 @@ static struct signal_info signal_set[] = {
 };
 
 
+static void error_bad_signal_number(SCM sig)
+{
+  STk_error("bad signal number ~S", sig);
+}
+
+/* ====================================================================== */
 static void sigint(int _UNUSED(i))
 {
   STk_signal("\n*** Interrupt ***\n");
@@ -145,6 +174,7 @@ static void sigabort(int _UNUSED(i))
   STk_error("Received a SIGABRT signal.");
 }
 
+/* ====================================================================== */
 int STk_get_signal_value(SCM sig)
 {
   const char *s;
@@ -154,7 +184,7 @@ int STk_get_signal_value(SCM sig)
 
   /* Search sig in the table of signal names */
   s = SYMBOL_PNAME(sig);
-  for (p = signal_set; p->name; p++)
+  for (p = signal_names; p->name; p++)
     if (strcasecmp(s, p->name) == 0) return p->value;
 Error:
   STk_error("bad signal name ~S", sig);
@@ -162,44 +192,107 @@ Error:
 }
 
 
-DEFINE_PRIMITIVE("%initialize-signals", initialize_signals, subr0, (void))
+static void exec_signal_handler(int i) // Run a Scheme procedure for signal i
+{
+  SCM proc = signals[i];
+
+  if (STk_procedurep(proc)) {
+    STk_C_apply(signals[i], 1, MAKE_INT(i));
+  }
+  else STk_panic("PROBLEM sith signal %d", i);
+}
+
+
+static void set_signal_handler(int sig, void(*proc)(int), int flag)
 {
   struct sigaction sigact;
 
-  /* ====  SIGSEGV  handler */
   sigfillset(&(sigact.sa_mask));
-  sigact.sa_handler = sigsegv;
-  sigact.sa_flags   = 0;
-  sigaction(SIGSEGV, &sigact, NULL);
+  sigact.sa_handler = proc;
+  sigact.sa_flags   = flag;
+  sigaction(sig, &sigact, NULL);
+}
 
-  /* ====  SIGINT  handler */
-  sigfillset(&(sigact.sa_mask));
-  sigact.sa_handler = sigint;
-  sigact.sa_flags   = 0;
-  sigaction(SIGINT, &sigact, NULL);
 
-  /* ====  SIGABRT  handler */
-  sigfillset(&(sigact.sa_mask));
-  sigact.sa_handler = sigabort;
-  sigact.sa_flags   = 0;
-  sigaction(SIGABRT, &sigact, NULL);
+DEFINE_PRIMITIVE("%default-signal-handler", dflt_sighdlr, subr1, (SCM sig))
+{
+  long n = STk_integer_value(sig);
+  if (n <= 0 || n >= NSIG)
+    STk_error("bad signal number ~S", sig);
 
-  /* ====  SIGHUP  handler */
-  sigfillset(&(sigact.sa_mask));
-  sigact.sa_handler = sighup;
-#ifdef SA_RESTART
-  sigact.sa_flags   = SA_RESTART;
-#else
-  sigact.sa_flags   = 0;
-#endif
-  sigaction(SIGHUP, &sigact, NULL);
+  switch (n) {
+    case SIGINT:  sigint(SIGINT);    break;
+    case SIGSEGV: sigsegv(SIGSEGV);  break;
+    case SIGABRT: sigabort(SIGABRT); break;
+    case SIGHUP:  sighup(SIGHUP);    break;
+  default: STk_error("signal ~S si not managed by this handler", sig);
+  }
+  return STk_void;
+}
+
+
+#define SET_DEFAULT_HANDLER(sig) do {                           \
+     signals[(sig)] = THE_PRIMITIVE(dflt_sighdlr);              \
+     set_signal_handler((sig), exec_signal_handler, SA_FLAGS);  \
+  } while (0);
+
+
+// This primitive is called when the REPL is initialized
+DEFINE_PRIMITIVE("%initialize-signals", initialize_signals, subr0, (void))
+{
+  SET_DEFAULT_HANDLER(SIGSEGV);
+  SET_DEFAULT_HANDLER(SIGINT);
+  SET_DEFAULT_HANDLER(SIGABRT);
+  SET_DEFAULT_HANDLER(SIGHUP);
 
   return STk_void;
 }
 
 
+DEFINE_PRIMITIVE("set-signal-handler!", set_sig_hdlr, subr2, (SCM sig, SCM proc))
+{
+  void(*p)(int);
+  long s = STk_integer_value(sig);
+
+  if (s <= 0 || s >= NSIG) error_bad_signal_number(sig);
+
+  if (BOOLEANP(proc)) 
+    p = (proc == STk_true)? SIG_DFL: SIG_IGN;
+  else {
+    if (STk_procedurep(proc) == STk_false)
+      STk_error("bad procedure ~S", proc);
+    p = exec_signal_handler;
+  }
+  signals[s] = proc;
+  set_signal_handler(s, p, SA_FLAGS);
+
+  return STk_void;
+}
+
+DEFINE_PRIMITIVE("get-signal-handler", get_sig_hdlr, subr1, (SCM sig))
+{
+  long s = STk_integer_value(sig);
+
+  if (s <= 0 || s >= NSIG) error_bad_signal_number(sig);
+  return signals[s];
+}
+
+
 int STk_init_signal()
 {
+  // Initialize the signals table
+  for (int i=0; i < NSIG; i++) {
+    signals[i] = STk_true;
+  }
+
+  // Define the symbols assiocated to signal names
+  for (struct signal_info *p = signal_names; p->name; p++) {
+    STk_define_variable(STk_intern(p->name), MAKE_INT(p->value), STk_STklos_module);
+  }
+
   ADD_PRIMITIVE(initialize_signals);
+  ADD_PRIMITIVE(set_sig_hdlr);
+  ADD_PRIMITIVE(get_sig_hdlr);
+
   return TRUE;
 }
