5842b6c167e4afc0b605605bc0894792c10e270b
Jeronimo Pellegrini <j_p@aleph0.info>
2023-02-15 16:24:23 -0300

    Procedures optionally remember their sources
    This patch does the same that was done to get docstrings and
    signatures in procedures.
    
    stklos> (compiler:keep-source #t)
    stklos> (define (f a b) (- a b))
    ;; f
    stklos> (%procedure-source f)
    (lambda (a b) (- a b))

--- src/vm-instr.h
+++ src/vm-instr.h
@@ -169,8 +169,9 @@
 #  define IN_FXLE              164
 #  define IN_FXGE              165
 #  define IN_FXDIFF            166
+#  define SOURCE               167
 
-#  define NB_VM_INSTR (IN_FXDIFF           +1)
+#  define NB_VM_INSTR (SOURCE              +1)
 #endif
 
 
@@ -344,6 +345,7 @@ static void *jump_table[] = {
   &&lab_IN_FXLE             ,
   &&lab_IN_FXGE             ,
   &&lab_IN_FXDIFF           ,
+  &&lab_SOURCE              ,
   NULL};
 #endif
 #undef DEFINE_JUMP_TABLE
@@ -519,6 +521,7 @@ static char *name_table[] = {
   "IN_FXLE             ",
   "IN_FXGE             ",
   "IN_FXDIFF           ",
+  "SOURCE              ",
   NULL};
 #endif
 #undef DEFINE_NAME_TABLE
--- src/vm.c
+++ src/vm.c
@@ -1541,6 +1541,15 @@ CASE(DOCSTRG) {
   NEXT;
 }
 
+CASE(SOURCE) {
+  SCM str = fetch_const();
+
+  if (vm->valc == 1 && CLOSUREP(vm->val))
+    CLOSURE_SOURCE(vm->val) = str;
+
+  NEXT;
+}
+
 CASE(CALL_LOCATION) {
    ACT_SAVE_INFO(vm->fp) = STk_cons(pop(),                   /* file */
                                     MAKE_INT(fetch_next())); /* line */
