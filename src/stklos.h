5d1152ab2e37dd05380c4f4237c52a6500607ea4
Jeronimo Pellegrini <j_p@aleph0.info>
2023-03-05 22:36:06 -0300

    Keep track of allocated bytes
    This patch:
    
    - Adds two fields to the VM structure, allocations and bytes_allocated.
      These are initialized when the thread starts.
    
    - Changes the standard STk_must_malloc and STk_must_malloc_atomic to
      increment those two counters for the current thread
    
    - Adds a %thread-allocations primitive that will return three values,
      the number of allocated bytes and the number of allocation calls
      since the thread started
    
    - Adds a %thread-allocations-reset! primitive that zeroes the two
      counters
    
    A helper function thread_inc_allocs(SCM thr, int size) was added to
    vm.c
    
    Example:
    
    ```
    stklos> (%thread-allocations (current-thread))
    2324467
    290558
    87388
    ```
    
    In this case, 5743784 bytes (290558 SCM objects) were allocated in
    219390 since the start of the thread.
    
    Caveat I: the number of allocations is kept in an unsigned long int,
    and there is no provision for recovering from overflow (it will
    wrap around).
    
    Caveat II: reallocations are not computed, since it would be a bit
    more complicated.

--- src/stklos.h
+++ src/stklos.h
@@ -155,8 +155,8 @@ extern "C"
   /* Scheme interface. *** THIS IS THE INTERFACE TO USE ***  */
 
 
-#define STk_must_malloc(size)           GC_MALLOC(size)
-#define STk_must_malloc_atomic(size)    GC_MALLOC_ATOMIC(size)
+#define STk_must_malloc(size)           STk_must_malloc_real(size)
+#define STk_must_malloc_atomic(size)    STk_must_malloc_atomic_real(size)
 #define STk_must_realloc(ptr, size)     GC_REALLOC((ptr), (size))
 #define STk_free(ptr)                   GC_FREE(ptr)
 #define STk_register_finalizer(ptr, f)  GC_REGISTER_FINALIZER( \
@@ -1450,6 +1450,18 @@ SCM STk_execute_C_bytecode(SCM consts, STk_instr *instr);
 int STk_init_vm(void);
 
 /*****************************************************************************/
+
+void thread_inc_allocs(SCM thr, int size);
+extern SCM STk_primordial_thread;
+
+static inline void* STk_must_malloc_real(size_t size) {
+  if (STk_primordial_thread) thread_inc_allocs(STk_current_thread(), size);
+  return GC_MALLOC(size);
+}
+static inline void* STk_must_malloc_atomic_real(size_t size) {
+  if (STk_primordial_thread) thread_inc_allocs(STk_current_thread(), size);
+  return GC_MALLOC_ATOMIC(size);
+}
 
 extern char *STk_boot_consts;
 extern STk_instr STk_boot_code[];
