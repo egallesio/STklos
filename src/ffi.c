9fac4ededd12385f46794c9dd970f01e68bdd8ee
Jeronimo Pellegrini <j_p@aleph0.info>
2024-08-23 16:44:56 -0300

    Allow absence of FFI
    This only prepares the C file `ffi.c`, but STklos still won't
    compile without FFI.
    
    To test this, comment out the FFI section in `conficure.ac`

--- src/ffi.c
+++ src/ffi.c
@@ -496,7 +496,7 @@ static int exec_callback(SCM callback, ...)
  * it is used in the gtk-gtklos-base ScmPkg (a callback can be called
  * with one or two pointers. Hopefully, callbacks were not documened
  * in previous versions of STklos ;-)
- * 
+ *
  * 2021/06/09: This code was used before for GTk support. It doesn't
  * seem useful anymore. It is kept here for reference
  */
@@ -703,7 +703,7 @@ DEFINE_PRIMITIVE("%make-ext-func", make_ext_func, subr4,
                  (SCM p1, SCM p2, SCM p3, SCM p4))
 { error_no_ffi(); return STk_void;}
 
-DEFINE_PRIMITIVE("make-callback", make_callback, subr3, (SCM p1, SCM p2, SCM p3))
+DEFINE_PRIMITIVE("%make-callback", make_callback, subr3, (SCM p1, SCM p2, SCM p3))
 { error_no_ffi(); return STk_void;}
 
 DEFINE_PRIMITIVE("%exec-callback-address", exec_cb_addr, subr0, (void))
@@ -729,9 +729,11 @@ DEFINE_PRIMITIVE("%set-typed-ext-var!", set_typed_ext_var, subr3,
  * ====================================================================== */
 int STk_init_ffi(void)
 {
+  #ifdef HAVE_FFI
   pointer_on_exec_callback = STk_make_Cpointer(exec_callback,
                                                STk_void,
                                                STk_false);
+  #endif
 
   ADD_PRIMITIVE(make_ext_func);
   ADD_PRIMITIVE(make_callback);
