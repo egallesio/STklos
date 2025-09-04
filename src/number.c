3707a3b8ece88369057b04bd68ad5963afffac92
Jeronimo Pellegrini <j_p@aleph0.info>
2025-09-04 11:30:22 -0300

    Handle infinities in complex sqrt
    This should happen:
    ```
         (sqrt 2.0-inf.0i)  => +inf.0-inf.0i
         (sqrt 2.0+inf.0i)  => +inf.0+inf.0i
         (sqrt +inf.0-2i)   => +inf.0-0.0i
         (sqrt +inf.0+2i)   => +inf.0+0.0i
         (sqrt -2.0-inf.0i) => +inf.0-inf.0i
         (sqrt -2.0+inf.0i) => +inf.0+inf.0i
         (sqrt -inf.0-2i)   => 0.0-inf.0i
         (sqrt -inf.0+2i)   => 0.0+inf.0i
    ```
    and ws not the case before this PR (some NaNs were retruned).

--- src/number.c
+++ src/number.c
@@ -4052,6 +4052,29 @@ static inline SCM my_sqrt_complex(SCM z)
   SCM a = COMPLEX_REAL(z);
   SCM b = COMPLEX_IMAG(z);
 
+  /* Handle infinities. For example,
+     (sqrt 2.0-inf.0i)  => +inf.0-inf.0i
+     (sqrt 2.0+inf.0i)  => +inf.0+inf.0i
+     (sqrt +inf.0-2i)   => +inf.0-0.0i
+     (sqrt +inf.0+2i)   => +inf.0+0.0i
+     (sqrt -2.0-inf.0i) => +inf.0-inf.0i
+     (sqrt -2.0+inf.0i) => +inf.0+inf.0i
+     (sqrt -inf.0-2i)   => 0.0-inf.0i
+     (sqrt -inf.0+2i)   => 0.0+inf.0i   */
+  if (IS_INFP(a)) {
+    double sign_of_b;
+    if (REALP(b))          sign_of_b = signbit(REAL_VAL(b));
+    else if (zerop(b))     sign_of_b = 0.0;
+    else if (positivep(b)) sign_of_b = +1.0;
+    else                   sign_of_b = -1.0;
+
+    return (REAL_VAL(a) > 0.0)
+      ? Cmake_complex(double2real(plus_inf), double2real(copysign(0.0, sign_of_b)))
+      : Cmake_complex(double2real(0.0), double2real(copysign(plus_inf, sign_of_b)));
+  }
+
+  if (IS_INFP(b)) return Cmake_complex(double2real(plus_inf), b);
+
   /* Given a, b we will compute A, B such that the square root of
      the complex number a+bi is
 
