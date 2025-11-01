f83ff9fb82fcadf22200dfae240dab6554c81096
Erick Gallesio <eg@stklos.net>
2025-11-01 17:19:41 +0100

    Fix: map or for-each with a non procedure did not yield an error

--- src/proc.c
+++ src/proc.c
@@ -384,6 +384,7 @@ static SCM map(int argc, SCM *argv, int in_map)
   argc -= 1;
   res   = STk_nil;
 
+  if (STk_procedurep(fct) == STk_false) error_bad_procedure(fct);
 
   if (argc == 1) {
     /* frequent case (map (lambda (x) ...) list). Do it specially */
